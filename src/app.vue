5ab00a8d2ea07aea4cbe4b942c22163b650ae0c6
yuanxin42 <1017123610@qq.com>
2020-04-07 10:39:35 +0800

    feat:添加图片loader，less解析

--- src/app.vue
+++ src/app.vue
@@ -2,12 +2,13 @@
  * @Author       : yuanxin42@xdf.cn
  * @Date         : 2020-04-03 10:05:55
  * @LastEditors  : yuanxin42@xdf.cn
- * @LastEditTime : 2020-04-03 16:39:59
+ * @LastEditTime : 2020-04-07 10:32:09
  * @Description  : 描述信息
  -->
 <template>
   <div id="title">
     <h1>{{demo()}}</h1>
+    <img src="./assets/bg1.png" alt="">
   </div>
 </template>
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,22 @@
  * @Author       : yuanxin42@xdf.cn
  * @Date         : 2020-04-03 10:05:55
  * @LastEditors  : yuanxin42@xdf.cn
- * @LastEditTime : 2020-04-03 16:39:20
+ * @LastEditTime : 2020-04-07 10:38:17
  * @Description  : 描述信息
  */
 // webpack.config.js
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 var path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+    chunkFilename: '[name].[hash].js',
+    publicPath:'/'
   },
   mode: 'development',
   module: {
@@ -35,13 +38,40 @@ module.exports = {
         loader: 'babel-loader'
       },
       {
+        test: /\.less$/,
+        loader: "style-loader!css-loader!less-loader",
+      },
+      {
         test: /\.css$/,
         use: [
-             process.env.NODE_ENV !== 'production'
+          process.env.NODE_ENV !== 'production'
             ? 'vue-style-loader'
             : MiniCssExtractPlugin.loader,
-              'css-loader'
-          ]
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8192
+          }
+        }]
+      },
+      {
+        test: /\.(eot|woff2?|ttf|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name]-[hash:5].min.[ext]',
+              limit: 5000,
+              publicPath: 'fonts/',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       }
     ]
   },
@@ -54,11 +84,16 @@ module.exports = {
   // compiler模式
   resolve: {
     alias: {
-        vue: 'vue/dist/vue.js',
+      vue: 'vue/dist/vue.js',
     }
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash:5].css',
+      allChunks: true
+    }),
     // 请确保引入这个插件来施展魔法
+    new CleanWebpackPlugin(),
     new VueLoaderPlugin(),
     // 添加html的插件
     new HtmlWebpackPlugin({
